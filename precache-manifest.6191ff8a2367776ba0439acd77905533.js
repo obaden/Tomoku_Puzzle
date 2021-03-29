a49f85aa846d8aa97b7f48fde16e68c91b5d6ec0
Oscar <oscarbaden@gmail.com>
2021-03-29 19:40:36 +1300

    Updates

---
+++ precache-manifest.6191ff8a2367776ba0439acd77905533.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29e0f5a3da5833b557ebd11f3ac07e45",
+    "url": "/Tomoku_Puzzle/index.html"
+  },
+  {
+    "revision": "bff60ad391deea049a02",
+    "url": "/Tomoku_Puzzle/static/css/main.4c4ebaa2.chunk.css"
+  },
+  {
+    "revision": "ab37bdbdd05590b666ca",
+    "url": "/Tomoku_Puzzle/static/js/2.db1afc7f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Tomoku_Puzzle/static/js/2.db1afc7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bff60ad391deea049a02",
+    "url": "/Tomoku_Puzzle/static/js/main.e7f8958e.chunk.js"
+  },
+  {
+    "revision": "5f3c360532243b3b0452",
+    "url": "/Tomoku_Puzzle/static/js/runtime-main.882ca137.js"
+  }
+]);
